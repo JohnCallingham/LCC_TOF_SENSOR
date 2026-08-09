15d0394d05f97063280afb1646332fd05aa77a80
John Callingham <john@callingham.com>
2026-08-09 11:13:07 +0100

    ToFSensorVL53L0X::read() returns 1000 instead of -1 for an error

--- src/ToFSensorVL53X0L.cpp
+++ src/ToFSensorVL53X0L.cpp
@@ -7,28 +7,6 @@ void ToFSensorVL53L0X::initialise(bool muxConnected) {
     return;
   }
 
-  // switch (connectionType) {
-  //   case ConnectionType::DIRECT:
-  //     Serial.printf("\n%6ld [ToFSensorVL53L0X::initialise] Initialising ToF sensor directly connected", millis());
-  //     if (! initialiseI2CDirect()) {
-  //       // No I2C device connected, so nothing to do!
-  //       return;
-  //     }
-  //     break;
-  //   case ConnectionType::MULTIPLEXOR:
-  //     Serial.printf("\n%6ld [ToFSensorVL53L0X::initialise] Initialising ToF sensor on multiplexor port %d", millis(), this->multiplexorPort);
-  //     if (! muxConnected) {
-  //       // No multiplexor connected, so nothing to do!
-  //       return;
-  //     }
-
-  //     if (! initialiseI2CMultiplexor(this->multiplexorPort)) {
-  //       // No I2C devices connected, so nothing to do!
-  //       return;
-  //     }
-  //     break;
-  // }
-
   // Check for a sensor on this port.
   if (! vl->begin()) {
     // There is no sensor on this port.
@@ -37,7 +15,6 @@ void ToFSensorVL53L0X::initialise(bool muxConnected) {
     } else {
       Serial.printf("\n%6ld [ToFSensorVL53L0X::initialise] No sensor directly connected", millis());
     }
-    // Serial.printf("\n%6ld [ToFSensorVL53L0X::initialise] No sensor on multiplexor port %d", millis(), this->multiplexorPort);
     sensorConnected = false;
     return;
   } 
@@ -47,7 +24,6 @@ void ToFSensorVL53L0X::initialise(bool muxConnected) {
   } else {
     Serial.printf("\n%6ld [ToFSensorVL53L0X::initialise] Sensor initialised directly connected", millis());
   }
-  // Serial.printf("\n%6ld [ToFSensorVL53L0X::initialise] Sensor on multiplexor port %d", millis(), this->multiplexorPort);
   sensorConnected = true;
 
   setInitialState();
@@ -59,7 +35,6 @@ int ToFSensorVL53L0X::read() {
 
   VL53L0X_RangingMeasurementData_t measure;
     
-  // Serial.print("Reading a measurement... ");
   vl->rangingTest(&measure, false); // pass in 'true' to get debug data printout!
   // vl->rangingTest(&measure, true); // pass in 'true' to get debug data printout!
 
@@ -70,45 +45,10 @@ int ToFSensorVL53L0X::read() {
     range = measure.RangeMilliMeter; 
   } else {
     // Serial.printf("\n out of range ");
-    range = -1;
+    // range = -1;
+    range = 1000;
   }
 
   // Serial.printf("\nStatus %d, Distance (mm): %d", rangeStatus, range);
   return range;
 }
-
-
-  // Serial.printf("\n%6ld [ToFSensorV53L0X::initialise] Initialising ToF sensor on multiplexor port %d", millis(), this->multiplexorPort);
-
-  // if (connectionType == ConnectionType::DIRECT) {
-
-  //   return;
-
-  // }
-
-  // if (connectionType == ConnectionType::MULTIPLEXOR) {
-
-  //   if (! muxConnected) {
-  //     // No multiplexor connected, so nothing to do!
-  //     return;
-  //   }
-
-  //   // if (! initialiseI2C()) {
-  //   if (! initialiseI2CMultiplexor(this->multiplexorPort)) {
-  //     // No I2C devices connected, so nothing to do!
-  //     return;
-  //   }
-
-  //   // Check for a sensor on this port.
-  //   if (! vl->begin()) {
-  //     // There is no sensor on this port.
-  //     Serial.printf("\n%6ld [ToFSensorVL53L0X::initialise] No sensor on multiplexor port %d", millis(), this->multiplexorPort);
-  //     sensorConnected = false;
-  //     return;
-  //   }
-
-  //   Serial.printf("\n%6ld [ToFSensorVL53L0X::initialise] Sensor on multiplexor port %d", millis(), this->multiplexorPort);
-  //   sensorConnected = true;
-  // }
-
-  // setInitialState();
